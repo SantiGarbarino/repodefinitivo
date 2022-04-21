a23e067ec40c0a23ec86b3e62a031e45b2a94dde
Santiago <sgarbarino@itba.edu.ar>
2022-04-21 14:29:43 -0300

    comienzo JS

---
+++ CLASE 6/codigo.js
@@ -0,0 +1,47 @@
+// resolver problemas logicos o matematico
+// la programacion es una herramienta para resolver problemas
+// 4 etapas del pensamiento computacional: 
+// descomposicion: descomponer el problema
+// patrones: similitudes
+// abstraccion: achicar el problema, eliminar lo superfluo
+// algoritmos:pasos para resolver el problema
+
+console.log("Hola mundo") //no funciono el run code. olvidarse.
+document.write('La web de Santi')
+
+/*
+Variables? Espacio en memoria donde se almacena un valor.
+Es como un cajon. Donde voy a guardar un dato.
+como armo una variable? comando let
+let nombreDeLaVariable = "valor" pj let nombre = "Santiago"
+el = es el operador de asignacion.
+
+Tipos de datos:
+numerico: enteros o con coma (integer o float)
+strings: cadena de caracteres
+boolean: true or false
+el signo de + sirve para la suma como para la concatenacion(unir strings)
+las "" identifican a los strings. si yo pongo "21" es un string.
+Nan significa Not a Number
+JScript usa por convencion la forma de escribir de camelCase
+(la primer palabra en miniscula y la otra arranca en mayuscula) pj: nombreApellido
+las constantes se escriben en MAYUSCULA.
+PascalCase: NombreApellido
+Snake_Case: nombre_apellido
+Kebab-Case_nombre-apellido
+para la division:
+si hago, document.write(10/2) me devuelve 5, me hace la division
+y devuelve el cociente
+si hago, document.write(10%2) me devuelve 0, devuelve el resto. como Python.
+
+cuando pongo condiciones:
+if...
+else if...
+else...
+el = asigna un valor a una variable
+el == pregunta si el valor coincide pj: ("11"==11) -->true
+el === pregunta si el valor y el tipo de dato coinciden pj: ("11"===11)-->false
+dentro de los if siempre se usa ===.
+hay que terminar cada renglon con un ;
+*/
+
