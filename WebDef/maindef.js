b31d480ced829145f55f771322107673aaa84d62
Santiago <sgarbarino@itba.edu.ar>
2022-05-24 20:42:09 -0300

    sace las lineas azules de los a href

--- WebDef/maindef.js
+++ WebDef/maindef.js
@@ -3,8 +3,22 @@
 titular1.innerText = "Nacionalidad: Argentina"
 */
 //PREGUNTAR CUAL ES LA DIFERENCIA ENTRE PONER innerText con poner innerHTML 
+function mostrarinf21(){
+    let contenedor = document.getElementById("comentario21")   //preguntar aca pq puse let contenedor
+    contenedor.innerHTML = "<ul> <li>Nacionalidad: Argentina</li> <li>Edad: 52 años</li> <li>Club: Atletico de Madrid (España)</li> </ul>" 
+}
 
- function mostrarinf1(){
-     let contenedor = document.getElementById("comentario1")   //preguntar aca pq puse let contenedor
-     contenedor.innerHTML = "<ul> <li>Nacionalidad: Argentina</li> <li>Edad: 24 años</li> <li>Club: Inter (Italia)</li>  <li>Posicion: Delantero centro</li> </ul>"
+ function mostrarinf22(){
+     let contenedor = document.getElementById("comentario22")   //preguntar aca pq puse let contenedor
+     contenedor.innerHTML = "<ul> <li>Nacionalidad: Argentina</li> <li>Edad: 44 años</li> <li>Seleccion: Argentina</li> </ul>"
  }
+
+function mostrarinf23(){
+    let contenedor = document.getElementById("comentario23")   //preguntar aca pq puse let contenedor
+    contenedor.innerHTML = "<ul> <li>Nacionalidad: Francia</li> <li>Edad: 49 años</li> <li>Club: Desempleado (-)</li></ul>"
+}
+
+function mostrarinf24(){
+    let contenedor = document.getElementById("comentario24")   //preguntar aca pq puse let contenedor
+    contenedor.innerHTML = "<ul> <li>Nacionalidad: Argentina</li> <li>Edad: 36 años</li> <li>Club: Racing Club (Argentina)</li></ul>"
+}
