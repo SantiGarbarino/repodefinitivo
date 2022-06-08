63f19b5f1972572ef9f9c01ac1fd91c426cd3926
Santiago <sgarbarino@itba.edu.ar>
2022-06-07 21:45:17 -0300

    casi terminado

--- Probando/prueba.js
+++ Probando/prueba.js
@@ -1,61 +1,42 @@
-function mostrarinf21(){
-    let contenedor = document.getElementById("comentario21")   
-    contenedor.innerHTML = "<ul> <li>Nacionalidad: Argentina</li> <li>Edad: 52 años</li> <li>Club: Atletico de Madrid (España)</li> </ul>"
+function enviarMail() {
+  let dedicar1 = document.getElementById("dedicar1").checked;
+  let dedicar2 = document.getElementById("dedicar2").checked; //preguntar pq cuando pongo dt no me lo reconoce
+  if (dedicar1 != true && dedicar2 != true) return alert("Debe elegir una opción.");
+  else
+    return alert(
+      "Hemos recibido su mail, en la brevedad nos estaremos comunicando con usted."
+    );
 }
 
- function mostrarinf22(){
-     let contenedor = document.getElementById("comentario22")   
-     contenedor.innerHTML = "<ul> <li>Nacionalidad: Argentina</li> <li>Edad: 44 años</li> <li>Seleccion: Argentina</li> </ul>"
- }
+const getRemainingTime = (deadline) => {
+  let now = new Date(),
+    remainTime = (new Date(deadline) - now + 1000) / 1000,
+    remainSeconds = ("0" + Math.floor(remainTime % 60)).slice(-2),
+    remainMinutes = ("0" + Math.floor((remainTime / 60) % 60)).slice(-2),
+    remainHours = ("0" + Math.floor((remainTime / 3600) % 24)).slice(-2),
+    remainDays = Math.floor(remainTime / (3600 * 24));
 
-function mostrarinf23(){
-    let contenedor = document.getElementById("info23")   // preguntar
-    contenedor.innerHTML = "<ul> <li>Nacionalidad: Francia</li> <li>Edad: 49 años</li> <li>Club: Desempleado (-)</li></ul>"
-}
+  return {
+    remainSeconds,
+    remainMinutes,
+    remainHours,
+    remainDays,
+    remainTime,
+  };
+};
 
-function mostrarinf24(){
-    let contenedor = document.getElementById("comentario24")   
-    contenedor.innerHTML = "<ul> <li>Nacionalidad: Argentina</li> <li>Edad: 36 años</li> <li>Club: Racing Club (Argentina)</li></ul>"
-}
+const countdown = (deadline, elem, finalMessage) => {
+  const el = document.getElementById(elem);
 
-function enviarMail(){
-    let dedicar = document.getElementById("dedicar").checked; //preguntar pq cuando pongo dt no me lo reconoce
-    if (dedicar != true)
-        return alert("Debe elegir una opción.");
-    else
-        return alert("Hemos recibido su mail, en la brevedad nos estaremos comunicando con usted.");
-}
+  const timerUpdate = setInterval(() => {
+    let t = getRemainingTime(deadline);
+    el.innerHTML = `${t.remainDays}D:${t.remainHours}H:${t.remainMinutes}M:${t.remainSeconds}s`;
 
-const getRemainingTime = deadline => {
-    let now = new Date(),
-        remainTime = (new Date(deadline) - now + 1000) / 1000,
-        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
-        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
-        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
-        remainDays = Math.floor(remainTime / (3600 * 24));
-  
-    return {
-      remainSeconds,
-      remainMinutes,
-      remainHours,
-      remainDays,
-      remainTime
+    if (t.remainTime <= 1) {
+      clearInterval(timerUpdate);
+      el.innerHTML = finalMessage;
     }
-  };
-  
-  const countdown = (deadline,elem,finalMessage) => {
-    const el = document.getElementById(elem);
-  
-    const timerUpdate = setInterval( () => {
-      let t = getRemainingTime(deadline);
-      el.innerHTML = `${t.remainDays}D:${t.remainHours}H:${t.remainMinutes}M:${t.remainSeconds}s`;
-  
-      if(t.remainTime <= 1) {
-        clearInterval(timerUpdate);
-        el.innerHTML = finalMessage;
-      }
-  
-    }, 1000)
-  };
-  
-  countdown('Nov 21 2022 07:00:00 GMT-0500', 'clock', '¡Ya empezó el Mundial!');
+  }, 1000);
+};
+
+countdown("Nov 21 2022 07:00:00 GMT-0500", "clock", "¡Ya empezó el Mundial!");
